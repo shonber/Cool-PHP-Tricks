2aac8ebc9451c5678b2dd7d8052b4ce7895b77bc
ShonBer <32219039+shonber@users.noreply.github.com>
2022-03-18 22:29:20 +0200

    Update Tricks.php

--- Tricks.php
+++ Tricks.php
@@ -1,1 +1,9 @@
+Trick (one line if-statement:
 <?php echo (isset($_GET["param"]) ? "The parameter exists" : "No such parameter"); ?>
+
+Explanation:
+<?php ?> ==> Is for starting a php code.
+echo ==> Printing data.
+(isset($_GET["param"]) ? ==> This is the condition of the if-statement.
+ "The parameter exists" : ==> If the condition is true this will execute (before the colon).
+ "No such parameter"); : ==> If the condition is false this will execute (after the colon).
